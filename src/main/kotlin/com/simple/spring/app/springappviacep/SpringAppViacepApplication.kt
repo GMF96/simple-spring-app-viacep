c401dde7088cc5440cbdaba25d94dd193289c1ab
Gustavo Frohlich <gustavo.frohlich@hotmail.com>
2021-03-29 10:36:07 -0300

    first commit to the project

---
+++ src/main/kotlin/com/simple/spring/app/springappviacep/SpringAppViacepApplication.kt
@@ -0,0 +1,11 @@
+package com.simple.spring.app.springappviacep
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAppViacepApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAppViacepApplication>(*args)
+}
